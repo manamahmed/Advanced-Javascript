dd1a3576505ade699bbc6a84db003088aa77da37
manamahmed <kazi.ahmed@stud.uni-due.de>
2021-02-02 20:44:39 +0100

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+
+function add(num1, num2){
+
+}
+const result = add(13);
+console.log(result);
+
+
+const premik = {name:"Smart Dude", phone:134554542 };
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
