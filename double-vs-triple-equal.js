ff3d6cdd3c736dbfc34d005939765ed4b5068561
manamahmed <kazi.ahmed@stud.uni-due.de>
2021-02-02 21:06:12 +0100

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,10 @@
+// == will check only value
+// === will check value and type
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
